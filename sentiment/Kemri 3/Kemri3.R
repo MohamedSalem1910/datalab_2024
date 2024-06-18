d466a0328a433948ece10c369ca6fe8e052b5d4b
Mohamed Salem <salemm0@sewanee.edu>
2024-06-18 10:58:50 -0500

    Kemri

---
+++ sentiment/Kemri 3/Kemri3.R
@@ -0,0 +1,11 @@
+# outcome of delivery (baby is okay and if baby was admitted to nenotolagy)
+# - malaria during pregnancy
+# - weight, height and apgar
+# - completeness of variables
+# - numbers of variables
+# - the mean age of women
+# - number of kids submitted to icu/neonatology
+# - who went to clinic and who did not go to clinic
+# - how many had a single or double or triple delivery
+# - mean birth weight and height and range & mean apgar and range
+# - difference time of when delivery was made (day vs. night)
